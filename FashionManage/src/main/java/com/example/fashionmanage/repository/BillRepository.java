f862b40b995279493012bf441ac7cbe5e6c66fef
Nhan <nguyenngocbaonhan2003@gmail.com>
2024-01-03 19:07:41 +0700

    update repo

--- FashionManage/src/main/java/com/example/fashionmanage/repository/BillRepository.java
+++ FashionManage/src/main/java/com/example/fashionmanage/repository/BillRepository.java
@@ -3,7 +3,6 @@ package com.example.fashionmanage.repository;
 import com.example.fashionmanage.entity.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
