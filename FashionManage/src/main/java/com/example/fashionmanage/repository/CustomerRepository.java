68ad774150ef17b2af5c85c30873ccd69dc5786e
Du0ngVanBa0 <tidumong789@gmail.com>
2024-01-04 00:15:38 +0700

    fix comment

--- FashionManage/src/main/java/com/example/fashionmanage/repository/CustomerRepository.java
+++ FashionManage/src/main/java/com/example/fashionmanage/repository/CustomerRepository.java
@@ -8,6 +8,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+    /**
+     * the function help search customer based on a string
+     *
+     * @param searchStr
+     * @return list Customer
+     * @author BaoDV
+     */
     @Query(value = "SELECT c.* FROM Customer c WHERE c.phone LIKE %:searchStr% OR c.name LIKE %:searchStr% OR c.id = :searchStr", nativeQuery = true)
     List<Customer> findAllByNameOrPhoneOrContainingId(@Param("searchStr") String searchStr);
 }
