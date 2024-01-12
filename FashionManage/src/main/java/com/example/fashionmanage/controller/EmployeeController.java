8de34948e8a042daf31c36e88a87ec765855b49c
thanhbui230202 <thanhbui.230202@gmail.com>
2024-01-12 20:16:45 +0700

    update

--- FashionManage/src/main/java/com/example/fashionmanage/controller/EmployeeController.java
+++ FashionManage/src/main/java/com/example/fashionmanage/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.fashionmanage.controller;
 
 import com.example.fashionmanage.dto.EmployeeDTO;
+import com.example.fashionmanage.entity.Employee;
+import com.example.fashionmanage.entity.User;
 import com.example.fashionmanage.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
