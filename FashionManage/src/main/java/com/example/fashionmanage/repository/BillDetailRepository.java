3b609bca1579b3eceb5f6afbed6f956b28b7cd9b
Nhan <nguyenngocbaonhan2003@gmail.com>
2024-01-19 11:08:48 +0700

    update

--- FashionManage/src/main/java/com/example/fashionmanage/repository/BillDetailRepository.java
+++ FashionManage/src/main/java/com/example/fashionmanage/repository/BillDetailRepository.java
@@ -3,5 +3,5 @@ package com.example.fashionmanage.repository;
 import com.example.fashionmanage.entity.BillDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BillDetailRepository extends JpaRepository<BillDetail, String> {
+public interface BillDetailRepository extends JpaRepository<BillDetail, Integer> {
 }
