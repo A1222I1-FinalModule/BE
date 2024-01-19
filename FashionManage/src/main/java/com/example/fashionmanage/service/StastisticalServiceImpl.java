269ca72ae69db8a94a2cbd3b1520d10410ada0dd
Nhan <nguyenngocbaonhan2003@gmail.com>
2024-01-19 19:29:44 +0700

    update importing service

--- FashionManage/src/main/java/com/example/fashionmanage/service/StastisticalServiceImpl.java
+++ FashionManage/src/main/java/com/example/fashionmanage/service/StastisticalServiceImpl.java
@@ -2,8 +2,12 @@ package com.example.fashionmanage.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.fashionmanage.repository.StatisticalRepository;
 
+@Service
 public class StastisticalServiceImpl implements StatisticalService {
   @Autowired
   private StatisticalRepository statisticalRepository;
--- FashionManage/src/main/java/com/example/fashionmanage/service/StatisticalService.java
+++ FashionManage/src/main/java/com/example/fashionmanage/service/StatisticalService.java
@@ -3,7 +3,6 @@ package com.example.fashionmanage.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface StatisticalService {
   List<Object[]> getStatisticalByMonth();
   List<Object[]> getStatisticalByDate();
