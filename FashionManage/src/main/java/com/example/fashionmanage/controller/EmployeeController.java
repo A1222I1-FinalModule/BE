786e6e8a03c4b2d6601bcee4cc2a29b1c600f033
thanhbui230202 <thanhbui.230202@gmail.com>
2024-01-25 18:09:38 +0700

    update

--- FashionManage/src/main/java/com/example/fashionmanage/controller/EmployeeController.java
+++ FashionManage/src/main/java/com/example/fashionmanage/controller/EmployeeController.java
@@ -1,14 +1,8 @@
 package com.example.fashionmanage.controller;
-
 import com.example.fashionmanage.dto.EmployeeDTO;
 import com.example.fashionmanage.entity.Employee;
 import com.example.fashionmanage.entity.User;
-import com.example.fashionmanage.service.EmployeeService;
 import com.example.fashionmanage.service.EmployeeServiceImpl;
-<<<<<<< HEAD
-=======
-
->>>>>>> bc2a796de048dac1b410b244bd52671479588768
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +11,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @CrossOrigin("*")
